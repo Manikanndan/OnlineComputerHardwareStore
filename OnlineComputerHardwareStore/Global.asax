7fbb2af42bdf140c3c2671b4be350e9e227adbc9
manikanndan <manikanndan.n15@gmail.com>
2019-08-22 21:46:51 +0530

    E-Commerce Project on Computer Hardware Store..

---
+++ OnlineComputerHardwareStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineComputerHardwareStore.MvcApplication" Language="C#" %>
---
+++ OnlineComputerHardwareStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineComputerHardwareStore.Startup))]
+namespace OnlineComputerHardwareStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
